cd248499dad42e0bbe3dbf58bd6500c4fb528da6
Michael Cipold <mci@jcbachmann.de>
2021-02-24 09:48:31 +0100

    Update dependencies and toolchain config
    Bump HunterGate and dependencies
    Require C++ 14
    Build with fPIC

---
+++ Release/cmake/toolchain.cmake
@@ -0,0 +1,7 @@
+# Ubuntu 18.04 ships with GCC 7 not fully supporting C++17
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+# Build hunter libs with fPIC, so that they can be used in PythonWrappers
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
