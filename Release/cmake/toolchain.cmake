44da08a67dc02cf1c5f0fa0494d219ffb313d0a8
Michael Cipold <mci@jcbachmann.de>
2021-02-25 00:41:37 +0100

    Set CMAKE_POSITION_INDEPENDENT_CODE option

--- Release/cmake/toolchain.cmake
+++ Release/cmake/toolchain.cmake
@@ -5,3 +5,5 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 
 # Build hunter libs with fPIC, so that they can be used in PythonWrappers
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
